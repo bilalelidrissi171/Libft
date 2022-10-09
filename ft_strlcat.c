fddb9ac23174964af8a1831368fbb1b4235694f7
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-09 21:25:16 +0100

    edit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 17:48:25 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/09 18:00:59 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/09 21:08:30 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,26 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 		y++;
 	}
 	dst[y] = '\0';
-	return (ft_strlen(src));
+
+	return (ft_strlen(src)+ft_strlen(dst));
+}
+
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char des[50] ;
+	char src[50] = "12345666";
+	printf("%lu\n",strlcat(des,src,0));
+	printf("des = %s\n",des);
+
+	char des1[50] = "12345";
+	char src1[50] = "12345666";
+	printf("%lu\n",ft_strlcat(des1,src1,3));
+	printf("des = %s",des1);
+
+
+
 }
