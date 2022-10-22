9c2b9ee189e9c888215007d3097cab5d99c72ffa
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-22 09:58:36 +0100

    edit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 16:01:26 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/22 09:17:51 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/22 09:53:29 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (!haystack && len == 0)
 		return (NULL);
 	lh = ft_strlen(haystack);
+
 	if (ft_strlen(needle) > len)
 		return (NULL);
-	while (i < len - 1 && i < lh)
+	if (needle[i] == '\0' && len == 0)
+		return ((char *)&haystack[i]);
+	while (i < len)
 	{
 		j = 0;
 		k = i;
@@ -45,20 +48,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
+// #include<stdio.h>
+// #include<string.h>
 
-
-#include<stdio.h>
-#include<string.h>
-
-int main()
-{
-	char haystack[10] = "abcd";
-	char needle[1] = "a";
-	printf("   strnstr : %s\n", strnstr(haystack,needle,1));
-	printf("ft_strnstr : %s\n", strnstr(haystack,needle,1));
+// int main()
+// {
+// 	char haystack[10] = "abcd";
+// 	char needle[1] = "a";
+// 	printf("   strnstr : %s\n", strnstr(haystack,needle,1));
+// 	printf("ft_strnstr : %s\n", ft_strnstr(haystack,needle,1));
 
 
 
-	return 0;
-}
-
+// 	return 0;
+// }
