2fb9dddb9d12a4ffef890ea10299ee88153647c7
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-23 08:50:34 +0100

    edit

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 09:55:41 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/23 05:57:03 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/23 08:43:11 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*p;
+
+	if (count == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
 	p = malloc(count * size);
-	if (p == NULL)
+	if (!p)
 		return (NULL);
 	ft_bzero(p,count * size);
 	return (p);
 }
+
+// #include <stdio.h>
+// int main()
+// {
+// 	printf("%p\n", calloc(2000000000 , 2000000000));
+// 	printf("%p\n", ft_calloc(2000000000 , 2000000000));
+// }
