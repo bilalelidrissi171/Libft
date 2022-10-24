80d5bf6c31a3e3956b6cd1580e0961e312e6026e
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-24 04:26:42 +0100

    edit

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bilal <bilal@student.42.fr>                +#+  +:+       +#+        */
+/*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 09:55:41 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/24 03:42:27 by bilal            ###   ########.fr       */
+/*   Updated: 2022/10/24 04:24:41 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,38 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	size_t	i;
 	void	*p;
+
+	i = -1;
+	if ((size > i / count && count))
+		return (NULL);
 	p = malloc(count * size);
 	if (!p)
 		return (NULL);
-	ft_memset(p,0,count * size);
+	ft_bzero(p,count * size);
 	return (p);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
 
-int main()
-{
-	size_t b = 0;
-	size_t i = 1;
-	size_t c = 1073741824;
-	size_t s = sizeof(char);
-	while (calloc(c * s))
-	{
-		// memset(ss,1,(c * s));
-		printf("%zu Gb\n",i++);
+// int main()
+// {
+// 	size_t b = 0;
+// 	size_t i = 1;
+// 	size_t c = 1073741824;
+// 	size_t s = sizeof(char);
+// 	while (memset(calloc(c , s),1,(c * s)))
+// 	{
+// 		;
+// 		printf("%zu Gb\n",i++);
 
-	}
-//hadi khbar zina
+// 	}
+// //hadi khbar zina
 
 
-	return 0;
-}
+// 	return 0;
+// }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,27 +6,27 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 12:41:08 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/24 00:43:15 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/24 04:23:25 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	i;
-	char	*str;
-	size_t	len_s1;
-	size_t	len_set;
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	size_t	i;
+// 	char	*str;
+// 	size_t	len_s1;
+// 	size_t	len_set;
 
-	i = 0;
-	len_s1 = ft_strlen(s1);
-	len_set = ft_strlen(set);
-	while()
-
-
+// 	i = 0;
+// 	len_s1 = ft_strlen(s1);
+// 	len_set = ft_strlen(set);
+// 	while()
 
 
 
 
-}
+
+
+// }
