ef3a359d2101e85112251f8744a3a4ca0dd66623
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-24 03:19:48 +0100

    edit

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 09:55:41 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/23 11:18:14 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/24 03:15:35 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,34 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*p;
-	size_t	for_max;
-
-	for_max = -1;
-	if (count >= for_max / size && size >= for_max / count)
-		return (NULL);
 	p = malloc(count * size);
 	if (!p)
 		return (NULL);
 	ft_bzero(p, count * size);
 	return (p);
+}
+
+#include <stdio.h>
+int main()
+{
+
+	size_t i = 1;
+	size_t c = 1073741824;
+	size_t s = sizeof(char);
+	while (1)
+	{
+		char *ss= malloc(c * s);
+		if (ss == NULL)
+		{
+			printf("Fin");
+			return 0;
+		}
+		memset(ss,1,(c * s));
+		printf("%zu Gb\n",i++);
+
+	}
+
+
+
+	return 0;
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,15 +6,23 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 12:41:08 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/23 12:47:58 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/24 00:43:15 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "stdio.h"
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	i;
 	char	*str;
+	size_t	len_s1;
+	size_t	len_set;
 
-
+	i = 0;
+	len_s1 = ft_strlen(s1);
+	len_set = ft_strlen(set);
+	while()
 
 
 
