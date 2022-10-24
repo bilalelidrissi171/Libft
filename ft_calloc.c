1cb20b05a59bdead90f4aa0c4d873c1a58c1f5af
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-24 04:07:54 +0100

    edit

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bilal <bilal@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 09:55:41 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/24 03:15:35 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/24 03:42:27 by bilal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,28 @@ void	*ft_calloc(size_t count, size_t size)
 	p = malloc(count * size);
 	if (!p)
 		return (NULL);
-	ft_bzero(p, count * size);
+	ft_memset(p,0,count * size);
 	return (p);
 }
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 int main()
 {
-
+	size_t b = 0;
 	size_t i = 1;
 	size_t c = 1073741824;
 	size_t s = sizeof(char);
-	while (1)
+	while (calloc(c * s))
 	{
-		char *ss= malloc(c * s);
-		if (ss == NULL)
-		{
-			printf("Fin");
-			return 0;
-		}
-		memset(ss,1,(c * s));
+		// memset(ss,1,(c * s));
 		printf("%zu Gb\n",i++);
 
 	}
-
+//hadi khbar zina
 
 
 	return 0;
