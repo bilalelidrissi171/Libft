5ee8995bcc17cd48ffaf12485a0e76010589e047
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-28 19:13:06 +0100

    edit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,23 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 12:41:08 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/28 18:26:11 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/28 19:12:55 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	when_start(char const *s1, char const *set)
+static size_t	ft_start(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	index;
 	size_t	count;
 
 	start = 0;
+	index = 0;
 	while (s1[start])
 	{
-		while (index < ft_strlen(set))
+		while (set[index])
 		{
 			if (s1[start] == set[index])
 				count++;
@@ -39,81 +40,47 @@ static size_t	when_start(char const *s1, char const *set)
 	return (start);
 }
 
-static size_t	when_end()
+static size_t	ft_end(char const *s1, char const *set)
 {
+	size_t	end;
+	size_t	index;
+	size_t	count;
 
+	count = 0;
+	index = 0;
+	end = ft_strlen(s1) - 1;
+	while (end >= 0)
+	{
+		while (set[index])
+		{
+			if (s1[end] == set[index])
+				count++;
+			index++;
+		}
+		if (count > 0)
+		{
+			index = 0;
+			count = 0;
+			end--;
+		}
+		else
+			break ;
+	}
+	return (end);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
+	char	*str;
 
+	if (!set)
+		return (ft_strdup(s1));
+	if (ft_start(s1, set) == ft_strlen(s1) - 1 || !s1 || s1[0] == '\0')
+		return (ft_strdup(""));
+	start = ft_start(s1, set);
+	end = ft_end(s1, set);
+	str = ft_substr(s1, start, end - start + 1);
+	return (str);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// char	*ft_strtrim(char const *s1, char const *set)
-// {
-// 	size_t	start;
-// 	size_t	len;
-// 	size_t	index_set;
-// 	size_t	count;
-
-// 	start = 0;
-// 	index_set = 0;
-// 	count = 0;
-// 	if (!set)
-// 		return (ft_strdup(s1));
-// 	if (!s1)
-// 		return (ft_strdup(""));
-// 	while (s1[start])
-// 	{
-// 		while (index_set < ft_strlen(set))
-// 		{
-// 			if (set[index_set] == s1[start])
-// 				count++;
-// 			index_set++;
-// 		}
-// 		if (count >= 1)
-// 		{
-// 			start++;
-// 			index_set = 0;
-// 			count = 0;
-// 		}
-// 		else
-// 			break ;
-// 	}
-// 	index_set = 0;
-// 	len = ft_strlen(s1) - 1;
-// 	while (len - start)
-// 	{
-// 		while (index_set < ft_strlen(set))
-// 		{
-// 			if (set[index_set] == s1[len])
-// 				count++;
-// 			index_set++;
-// 		}
-// 		if (count >= 1)
-// 		{
-// 			len--;
-// 			index_set = 0;
-// 			count = 0;
-// 		}
-// 		else
-// 			break ;
-// 	}
-// 	if (len < start)
-// 		return (ft_strdup(""));
-// 	len = len - start + 1;
-// 	return (ft_substr(s1, start, len));
-// }
