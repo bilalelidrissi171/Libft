defc18e78c093abe5d405ba9fe5707e162b9d858
Bilal El Idrissi <bilalelidrissi171@gmail.com>
2022-10-29 22:34:42 +0100

    edit

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,22 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 12:44:05 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/29 12:44:06 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/29 22:32:05 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (!*lst)
+		return ;
+
+	t_list *l;
+	l = *lst;
+	while (l)
+	{
+		ft_lstdelone(l,del);
+		l = l->next;
+	}
+}
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,17 @@
 /*   By: bel-idri <bel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 12:44:11 by bel-idri          #+#    #+#             */
-/*   Updated: 2022/10/29 12:44:12 by bel-idri         ###   ########.fr       */
+/*   Updated: 2022/10/29 22:24:41 by bel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst)
+		return ;
+
+	del(lst->content);
+	free(lst);
+}
